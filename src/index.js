4e24436cf8febde8698aab70d51fb7c69e3ee995
MaryiaBond <dairymolko@gamil.com>
2020-02-25 17:35:42 +0300

    feat: complete typical array problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined)
+        return 0
+    return array.reduce((min, n) => min > n ? min = n : min, 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined)
+        return 0
+    return array.reduce((min, n) => min < n ? min = n : min, 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length === 0)
+        return 0
+    let total = array.reduce((total, n) => total+=n, 0);
+    return total/array.length
 }
